beaca209b640c0b8130b5e3c595b75abdd7d73a5
Ashwani Kumar <60794217+ashwthama@users.noreply.github.com>
2024-01-02 23:10:30 +0530

    initial code push

---
+++ HRMS/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public string GetUser()
+        {
+            return "User";
+        }
+    }
+}

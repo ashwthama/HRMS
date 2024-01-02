fc35dcf41d53334c94309885f34f7c5dec78d86a
Ashwani Kumar <60794217+ashwthama@users.noreply.github.com>
2024-01-02 23:40:02 +0530

    added layers and employee atributes

---
+++ Domain/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    class Employee
+    {
+        public string EmpId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string BloodGroup { get; set; }
+        public string AadharNo { get; set; }
+        public string PANNo { get; set; }
+        public string ProfileImg { get; set; }
+        public string Experience {  get; set; }
+        public List<Tech> Techs { get; set; }
+
+
+    }
+    class Tech
+    {
+        public string TechName {  get; set; }
+        public double TechExperience { get; set;}
+    }
+}
